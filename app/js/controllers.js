0ca7b69cb927843e91cbed0a8ccf7d9dd36abee8
Christoph Dietze <git@cdietze.de>
2013-05-10 03:05:57 +0200

    basic UI: showing input and output word lists…

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,34 @@
 'use strict';
 
 /* Controllers */
-
+/*
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('ngramController', [function($scope) {
+  	$scope.inputWords = ["Hallo", "Nexus", "Katharina", "Linux", "Scala"];
 
   }])
   .controller('MyCtrl2', [function() {
 
   }]);
+
+  */
+
+function NgramController($scope) {
+	$scope.inputWords = ["Hallo", "Nexus", "Katharina", "Linux", "Scala", "Hallo2", "Nexus2", "Katharina2", "Linux2", "Scala2"];
+	$scope.outputWords = $scope.inputWords
+	$scope.wordToAdd = "";
+
+	$scope.addWord = function() {
+		if($scope.wordToAdd === "") return;
+		if($.inArray($scope.wordToAdd, $scope.inputWords) != -1) return;
+		$scope.inputWords.push($scope.wordToAdd);
+		$scope.wordToAdd = "";
+	}
+
+	$scope.removeWord = function(word) {
+		console.info("removing: "+word);
+		var index = $.inArray(word, $scope.inputWords);
+		$scope.inputWords.splice(index, 1);
+	}
+
+}
